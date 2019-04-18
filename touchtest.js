c6829b43232c5cbebe0c5c0c77b1b099d0f0589f
Jacob Moulton <Jacob.Moulton@gmail.com>
2019-04-18 13:47:48 -0600

    get right image plus notes

--- touchtest.js
+++ touchtest.js
@@ -79,19 +79,22 @@ function showWebsite(){
 Objective 3:
 Get the company images based on the CompanyId from Objective 1. Print out the “PublicUrl” of the image with the id of 5.
 */
-function getImages(){
+function getImage(){
     if(!checkToken()){
         return
     }
     $.ajax({
-        url : base + "/companies/"+companyID,
+        url : `${base}/companies/${companyID}/images`,
         method : 'GET',
         headers : {
             "Authorization": "Bearer "+token,
             "Content-Type":"application/json"
         },
         success : function(data,status,_){
-            output(`<a href="${data.website}">${data.website}</a>`)
+            var imageUrl = data.find(imgEntry => imgEntry.Id == 5).PublicUrl
+            output(`<img src="${imageUrl}"><p>${imageUrl}</p>`)
+            //this would work too, but what if IDs didn't match indexes?
+            //output(`<img src="${data[5].PublicUrl}">`)
         }
     })
 }
